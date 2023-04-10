55920f64273f8764299912efb5e81d3d9e75b88f
tildejustin <spacepepper3.14@gmail.com>
2023-04-09 22:30:20 -0400

    why was this class implemented in the first place

--- src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/set/spawn/mod/mixin/ServerPlayerEntityMixin.java
@@ -1,8 +1,6 @@
 package net.set.spawn.mod.mixin;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.container.ContainerListener;
-import net.minecraft.entity.player.PlayerEntity;
+import com.mojang.authlib.GameProfile;import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -19,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends PlayerEntity implements ContainerListener {
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @Shadow @Final public MinecraftServer server;
 
